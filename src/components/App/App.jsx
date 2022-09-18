a4005385b5689f333b34a4b395068967ea9ae070
Mark <markfamiliytitan@gmail.com>
2022-09-18 18:32:38 +0300

    test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,7 +13,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
+        <Route path="//" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
