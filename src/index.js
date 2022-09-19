49ea850f9f697412e3ff22619d3af707e95a84f1
Mark <markfamiliytitan@gmail.com>
2022-09-19 17:56:27 +0300

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
-      basename="/mark4titan.github.io/"
+      basename="/goit-react-hw-05-movies"
     >
       <App />
     </BrowserRouter>
